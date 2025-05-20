48cff86c94cb34856200f73056dd13fd6e8b261f
Amand95 <amanda.barros@fiap.com.br>
2025-05-20 11:45:35 -0300

    Update database.py

--- python/python/database.py
+++ python/python/database.py
@@ -1,36 +1,24 @@
 import sqlite3
 
-def create_connection(db_file):
-    """
-    Cria uma conexão com o banco de dados SQLite especificado por db_file.
-    Retorna o objeto conexão.
-    """
+def criar_conexao(db_file='farmtech.db'):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print("Conexão com banco de dados SQLite estabelecida.")
+        print("Conexão ao banco de dados SQLite estabelecida.")
     except sqlite3.Error as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
     return conn
 
-def create_tables(conn):
-    """
-    Cria as tabelas necessárias para armazenar as leituras dos sensores e o estado da bomba.
-    """
+def criar_tabelas(conn):
     try:
         cursor = conn.cursor()
-
-        # Tabela para armazenar as leituras dos sensores
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS leituras (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                umidade REAL NOT NULL,
-                ph REAL NOT NULL,
-                fosforo BOOLEAN NOT NULL,
-                potassio BOOLEAN NOT NULL,
-                estado_rele BOOLEAN NOT NULL,
-                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-            )
+        CREATE TABLE IF NOT EXISTS sensores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo TEXT NOT NULL,
+            valor REAL NOT NULL,
+            data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
         ''')
         conn.commit()
         print("Tabelas criadas com sucesso.")
